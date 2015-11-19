8160c374bbf40ac3849c7d86c1989401c833295e
George Merlocco <george@merloc.co>
2015-11-19 01:05:05 -0500

    Refactored entire app based on https://github.com/Bencodes/Wallpaper-Manager; Converted to Gradle build system

---
+++ app/src/main/java/com/team/broccoli/pcb/wallpapers/activity/LauncherActivity.java
@@ -0,0 +1,16 @@
+package com.team.broccoli.pcb.wallpapers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class LauncherActivity extends SherlockFragmentActivity {
+
+	@Override
+	public void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		final Intent intent = new Intent(this, HomeActivity.class);
+		super.startActivity(intent);
+	}
+
+}
---
+++ app/src/main/java/com/team/broccoli/pcb/wallpapers/activity/LoadingFragment.java
@@ -0,0 +1,18 @@
+package com.team.broccoli.pcb.wallpapers;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class LoadingFragment extends SherlockFragment {
+
+	public static final String TAG = "RetryFragment";
+
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.fragment_loading, null, false);
+	}
+}
